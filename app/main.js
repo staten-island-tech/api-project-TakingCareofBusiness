15089f2c9d6638b1d7550228bc8938ddedb18d6b
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-15 11:35:46 -0500

    huh

--- app/main.js
+++ app/main.js
@@ -1,1 +1,7 @@
 import "./style.css";
+const URL = "https://bugsnaxapi.com/api/bugsnax";
+async function getData(URL) {
+  const response = await fetch(URL);
+  console.log(response);
+}
+getData(URL);
