dc3d5215f128e216024dbfa02581871f51439d3e
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-15 23:48:38 -0500

    hmmm

--- app/main.js
+++ app/main.js
@@ -3,8 +3,9 @@ const URL = "https://fortnite-api.com/v2/cosmetics/br";
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const info = await response.json();
-    console.log(info.data);
+    const jsonDirect = await response.json();
+    const info = JSON.parse(jsonDirect);
+    console.log(info.data[0]);
   } catch (error) {
     console.log(error);
   }
