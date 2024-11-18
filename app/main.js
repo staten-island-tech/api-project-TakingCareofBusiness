a0460d2a7eccd0a6b7087e4cdadf5bcaeaad8bb9
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-18 12:32:28 -0500

    Its not a moon

--- app/main.js
+++ app/main.js
@@ -1,13 +1,32 @@
 import "./style.css";
 const URL = "https://fortnite-api.com/v2/cosmetics/br";
+const DOMSelectors = {
+  cardContainer: document.querySelector(".card-container"),
+};
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const jsonDirect = await response.json();
-    const info = JSON.parse(jsonDirect);
-    console.log(info.data[0]);
+    const cosmetics = await response.json();
+    const skins = Object.values(cosmetics.data).filter(
+      (cosmetic) => cosmetic.type.displayValue === "Outfit"
+    );
+
+    console.log(skins);
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
+function createCard(array) {
+  let x = 0;
+  while (x < 20) {
+    array.forEach(
+      (album) =>
+        DOMSelectors.cardContainer.insertAdjacentHTML(
+          "beforeEnd",
+          `<div class="card"><div class="header-container"><h2>${album.name}</h2></div><img src=${album.cover} alt="${album.name} cover" class="card-image"><div class="info-container"><p class="card-artist">${album.artist}</p><p class="card-genre">Genre: ${album.genre}</p><p class="card-date"> Date Released: ${album.releaseDate}</p><p class="card-length"> Album Length: ${album.albumLength} minutes</p><p class="card-amount">Number of Songs: ${album.albumAmount}</p></div></div>`
+        ),
+      (x += 1)
+    );
+  }
+}
