52d1346279e4247aeca42ff5c116c8a3f14f0fe6
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-26 12:31:01 -0500

    This is complicated af

--- app/main.js
+++ app/main.js
@@ -11,29 +11,36 @@ async function getData(URL) {
       (cosmetic) => cosmetic.type.displayValue === "Outfit"
     );
     console.log(skins);
-    createCard(skins, 0);
+    createCard(skins);
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
-function createCard(array, limit) {
-  if (limit === 0) {
-    limit = 3000;
-  }
+function createCard(array) {
   let x = 0;
-  while (x < limit) {
+  while (x <= array.length) {
     if (
       !(array[x].description === "TBD") &&
       !(array[x].name === "Stormfarer") &&
       !(array[x].name === "Set_01_TA_SG") &&
-      !(array[x].name === "Set_01_LA_SG")
+      !(array[x].name === "Set_01_LA_SG") &&
+      !(array[x].description === "NPC")
     ) {
       if (array[x].images.icon && array[x].introduction) {
+        if (array[x].description.indexOf("Reactive") > -1) {
+          let splitThing = array[x].description.split("\\");
+          let descript = splitThing[0];
+        } else {
+          let descript = array[x].description;
+        }
         DOMSelectors.cardContainer.insertAdjacentHTML(
           "beforeEnd",
-          `<div class="card w-[30%] bg-white mx-auto flex flex-wrap justify-evenly"><div class="header-container"><h2>${array[x].name}</h2></div><img src=${array[x].images.icon} alt="${array[x].name}'s Fortnite skin" class="card-image w-[90%]"><div class="info-container"><p class="skin-desc">${array[x].description}</p><p class="skin-rarity">Skin rarity: ${array[x].rarity.displayValue}</p><p class="skin-release">${array[x].introduction.text}</p></div></div>`
+          `<div class="card w-[30%] bg-white mx-auto flex flex-wrap justify-evenly"><div class="header-container"><h2>${array[x].name}</h2></div><img src=${array[x].images.icon} alt="${array[x].name}'s Fortnite skin" class="card-image w-[90%]"><div class="info-container"><p class="skin-desc">${descript}</p><p class="skin-rarity">Skin rarity: ${array[x].rarity.displayValue}</p><p class="skin-release">${array[x].introduction.text}</p></div></div>`
         );
+      }
+      if (array[x].name === "Loveless") {
+        x = array.length;
       }
       x += 1;
       console.log(array[x]);
